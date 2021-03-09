bf7a80e326aa00267e1728b2b356fd71b50f857b
Jen-Chieh <jcs090218@gmail.com>
2021-03-09 11:53:50 +0800

    Add core

---
+++ keytar.el
@@ -0,0 +1,38 @@
+;;; keytar.el --- Interface for node-keytar  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Shen, Jen-Chieh
+;; Created date 2021-03-09 11:52:53
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Interface for node-keytar.
+;; Keyword: keytar password credential secret security
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/keytar
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Interface for node-keytar.
+;;
+
+;;; Code:
+
+
+
+(provide 'keytar)
+;;; keytar.el ends here
