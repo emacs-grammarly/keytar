644e6243eef932375034d7b6d81f6607d7b122b3
Jen-Chieh <jcs090218@gmail.com>
2021-03-09 13:17:42 +0800

    Bump for initial release

--- keytar.el
+++ keytar.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Interface for node-keytar.
 ;; Keyword: keytar password credential secret security
-;; Version: 0.0.1
+;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24.3"))
 ;; URL: https://github.com/emacs-grammarly/keytar
 
