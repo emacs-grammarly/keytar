5b3501dc95755d85fcf21b00ae0d347446efe73b
JenChieh <jcs090218@gmail.com>
2025-01-01 00:49:59 -0800

    Update copyright year

--- keytar.el
+++ keytar.el
@@ -1,6 +1,6 @@
 ;;; keytar.el --- Emacs Lisp interface for node-keytar  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2021-2024  Shen, Jen-Chieh
+;; Copyright (C) 2021-2025  Shen, Jen-Chieh
 ;; Created date 2021-03-09 11:52:53
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
